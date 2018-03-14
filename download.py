efdb15d16ff9aeed8247f353eacf3720f8c29cf8
NOKUBI Takatsugu <knok@daionet.gr.jp>
2018-03-14 15:46:08 +0900

    fix url

--- download.py
+++ download.py
@@ -10,7 +10,7 @@ mnist_urls = [
     "http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz",
     "http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz",
     "http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz",
-    "http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz"
+    "http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz"
 ]
 
 def get_args():
